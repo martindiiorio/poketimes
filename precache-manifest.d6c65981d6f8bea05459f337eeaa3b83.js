8d458b9da4ab404ff84e0cb99834623b495a8814
Martin Di iorio <martin@coderteams.com>
2019-01-31 00:06:59 -0300

    Updates

---
+++ precache-manifest.d6c65981d6f8bea05459f337eeaa3b83.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f10bf0780aecd8ff951259976c1784dd",
+    "url": "/poketimes/static/media/pokeball.f10bf078.png"
+  },
+  {
+    "revision": "7b792e93ca24609537ee",
+    "url": "/poketimes/static/js/runtime~main.7b792e93.js"
+  },
+  {
+    "revision": "6c5fcac6fb0340c91101",
+    "url": "/poketimes/static/js/main.6c5fcac6.chunk.js"
+  },
+  {
+    "revision": "2dc14526f13c19f1ab61",
+    "url": "/poketimes/static/js/1.2dc14526.chunk.js"
+  },
+  {
+    "revision": "6c5fcac6fb0340c91101",
+    "url": "/poketimes/static/css/main.a0921d22.chunk.css"
+  },
+  {
+    "revision": "d68c788f76a542fdad706b606c6420ce",
+    "url": "/poketimes/index.html"
+  }
+];
