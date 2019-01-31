fe883bff86181d07dff8c73a438145a93714df88
Martin Di iorio <martin@coderteams.com>
2019-01-30 23:34:09 -0300

    Initial commit

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Rainbow from '../hoc/Rainbow';
+
+const About = () => {
+  return (
+    <div className="container">
+      <h4 className="center">About</h4>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis eos delectus reiciendis temporibus aut, molestiae dignissimos veritatis ex qui dolore nobis nihil ea incidunt quidem, iure tempore veniam deleniti nesciunt.</p>
+    </div>
+  )
+}
+
+export default Rainbow(About);
---
+++ src/components/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Contact = (props) => {
+  return (
+    <div className="container">
+      <h4 className="center">Contact</h4>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis eos delectus reiciendis temporibus aut, molestiae dignissimos veritatis ex qui dolore nobis nihil ea incidunt quidem, iure tempore veniam deleniti nesciunt.</p>
+    </div>
+  )
+}
+
+export default Contact;
